a68a9e99e394ac4c7bcea7b0cedae239caa34316
Constaa <jonas.dietsche@gmx.net>
2021-03-17 15:37:09 +0100

    kalender hinzugefügt

--- src/main/java/dhbw/server/controller/KalenderController.java
+++ src/main/java/dhbw/server/controller/KalenderController.java
@@ -1,7 +1,9 @@
 package dhbw.server.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class KalenderController {
 
     @GetMapping("/kalender")
