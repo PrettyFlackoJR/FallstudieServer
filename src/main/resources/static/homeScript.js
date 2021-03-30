bc164d8e8c83fdd62a8434fb509e1150c5368010
Elias <e.mueller24@gmx.de>
2021-03-30 12:48:09 +0200

    JS

--- src/main/resources/static/homeScript.js
+++ src/main/resources/static/homeScript.js
@@ -1,16 +1,23 @@
 const addButton = document.getElementById("add");
 addButton.addEventListener("click", weiterleitenZuTermin)
+const endButton = document.getElementById("endPlanningtime");
 
 async function onLoad() {
-    var calendarEl = document.getElementById('calendar');
-    const url = "http://localhost:8080/vorlesungsplaner/process_kalender";
+    kursNeuladen();
+    showButtons();
+    loadTable();
+}
+async function showButtons() {
+    const url = "http://localhost:8080/process_role";
     const res = await fetch(url);
     const json = await res.json();
 
-    var calendar = new FullCalendar.Calendar(calendarEl, json);
-
-    calendar.render();
-    kursNeuladen();
+    json.forEach(x => {
+        if (x === 3) {
+            endButton.style.display = "inline-block";
+            addButton.style.display = "inline-block";
+        }
+    })
 }
 async function kursNeuladen() {
     var calendarEl = document.getElementById('calendar');
@@ -39,5 +46,34 @@ async function weiterleitenZuTermin() {
     } else {
         const url = "http://localhost:8080/vorlesungsplaner/termin_add?kurs=" + kursValue;
         location.href = url;
+    }
+}
+async function endPlanning() {
+    const url = "http://localhost:8080/vorlesungsplaner/process_endplanning";
+    const res = await fetch(url);
+    alert("Sie haben Ihre Planung beendet.");
+    endButton.style.display="none";
+    addButton.style.display="none";
+}
+async function loadTable() {
+    const table = document.getElementById("tabelle");
+    const kurs = document.getElementById("kurs1");
+    const kursValue = kurs.options[kurs.selectedIndex].value;
+    const url = "http://localhost:8080/vorlesungsplaner/process_kalendertable?kurs=" + kursValue;
+
+    const res = await fetch(url);
+    const json = await res.json();
+
+    for (let i = 1; i < json.length+1; i++) {
+        let row = table.insertRow(i);
+        let cell1 = row.insertCell(0);
+        let cell2 = row.insertCell(1);
+        let cell3 = row.insertCell(2);
+        let cell4 = row.insertCell(3);
+
+        cell1.innerHTML = json[i].name;
+        cell2.innerHTML = json[i].termin.ter_datum;
+        cell3.innerHTML = json[i].termin.ter_start;
+        cell4.innerHTML = json[i].termin.ter_ende;
     }
 }
