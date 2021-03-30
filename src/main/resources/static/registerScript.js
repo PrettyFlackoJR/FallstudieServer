e4b5f86ba641f7ab37cda1126dc858882699950e
Jonas <jonas.dietsche@gmx.net>
2021-03-30 19:52:02 +0200

    commit

--- src/main/resources/static/registerScript.js
+++ src/main/resources/static/registerScript.js
@@ -1,6 +1,7 @@
 const addMore = document.getElementById("moreFields");
 const list = document.querySelector(".item-list");
-addMore.addEventListener("click", moreFields);
+addMore.addEventListener("click", moreFields)
+list.addEventListener("click", trashErledigt);
 
 var array= [];
 let counter = 0;
@@ -13,6 +14,17 @@ async function moreFields() {
         alert("Sachen eingeben");
         return false;
     }
+    if (stundenValue < 1) {
+        alert("Stunden dürfen nicht negativ oder 0 sein.");
+        return false;
+    }
+    for(const a of array) {
+        if(a.vorlesungsId == vorlesungValue) {
+            alert("Vorlesungen dürfen nicht doppelt vorkommen.");
+            return false;
+        }
+    }
+
     obj = {
         kursId: kursValue,
         vorlesungsId: vorlesungValue,
@@ -23,6 +35,26 @@ async function moreFields() {
     document.getElementById("vorlesungsListe").value = "";
     document.getElementById("stunden").value = "";
 
+    const body = JSON.stringify(array);
+ /*   const res = await fetch("/register_process/" +title, {
+        method: "POST",
+        headers: {
+            "Accept": "application/json",
+            "Content-Type": "application/json"
+        },
+        body: body
+    });
+    if(res.status == 400){
+        alert("Bitte Eintrag machen.");
+    }*/
+
+    loadItems();
+
+
+}
+async function loadItems() {
+
+    clearChildren(list);
     for (const a of array) {
         const itemsDiv = document.createElement("div");
         itemsDiv.classList.add("items")
@@ -48,8 +80,34 @@ async function moreFields() {
         list.appendChild(itemsDiv);
     }
 }
+function clearChildren(element) {
+    while (element.firstElementChild != null) {
+        element.removeChild(element.firstElementChild);
+    }
+}
 function myFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
+}
+async function trashErledigt(e) {
+    const item = e.target;
+    //ITEM loeschen
+    if (item.classList[0] == "trash-btn" || item.classList.value == "fas fa-trash") {
+        let eintrag = item.parentElement;
+        if (item.classList.value == "fas fa-trash") {
+            eintrag = eintrag.parentElement;
+        }
+        eintrag.classList.add("fall");
+
+// get index of object with id:37
+        var removeIndex = array.map(function(item) { return item.vorlesungsId; }).indexOf(eintrag.childNodes[1].value);
+
+// remove object
+        array.splice(removeIndex, 1);
+        eintrag.addEventListener("transitionend", async function () {
+            eintrag.remove();
+        });
+        loadItems();
+    }
 }
 async function eingabeUeberpruefen() {
     let kurs = document.getElementById("kursListe");
